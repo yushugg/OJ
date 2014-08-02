7bc72230fe6551469ae45020bd165d016d83fb19
yushu <mahuachungg@gmail.com>
2014-08-02 11:56:17 +0800

    pat advance 1074 added, not all the nodes are in this list

--- pat_advance/pat_advance_1074.cc
+++ pat_advance/pat_advance_1074.cc
@@ -17,6 +17,7 @@ struct cmp_str
 };
 std::map<const char*, Node, cmp_str> list;
 
+// !Perhaps some input nodes are not in the list
 int main()
 {
   freopen("in.txt", "r", stdin);
@@ -26,16 +27,80 @@ int main()
 
   for (int i = 0; i < N; ++i)
   {
-    char addr[6];
+    char* addr = new char[6];
     Node node;
     scanf("%s%d%s", addr, &node.data, node.next);
     list.insert(std::make_pair(addr, node));
   }
 
+  // Calculate the real N
+  char test[6];
+  strcpy(test, root);
+  N = 0;
+  while (strcmp(test, "-1"))
+  {
+    strcpy(test, list[test].next);
+    ++N;
+  }
+
+  char newRoot[6];
+  strcpy(newRoot, root);
+  char start[6];
+  strcpy(start, root);
+  char preEnd[6];
+  for (int i = 0; i+K <= N; i += K)
+  {
+    int cnt = 0;
+    char end[6];
+    strcpy(end, start);
+    // Find end, ie. next loop's start
+    while (cnt < K)
+    {
+      strcpy(end, list[end].next);
+      if (cnt == K-2)
+      {
+        if (i == 0)
+          strcpy(newRoot, end);
+        else
+          strcpy(list[preEnd].next, end);
+      }
+      ++cnt;
+    }
+
+    char prev[6];
+    strcpy(prev, end);
+    char now[6];
+    strcpy(now, start);
+    cnt = 0;
+    while(cnt < K)
+    {
+      char post[6];
+      strcpy(post, list[now].next);
+      strcpy(list[now].next, prev);
+      // Update now, post
+      strcpy(prev, now);
+      strcpy(now, post);
+      ++cnt;
+    }
+
+    // Update start
+    strcpy(preEnd, start);
+    strcpy(start, end);
+  }
+
   // Iterate
-  printf("%d\n", list.size());
-  for (std::map<const char*, Node, cmp_str>::iterator iter = list.begin(); iter != list.end(); ++iter)
-    printf("%s %d %s\n", iter->first, (iter->second).data, (iter->second).next);
+  //printf("%d\n", list.size());
+  int cnt = 0;
+  char begin[6];
+  strcpy(begin, newRoot);
+  while (cnt < N)
+  {
+    printf("%s %d %s\n", begin, list[begin].data, list[begin].next);
+    strcpy(begin, list[begin].next);
+    ++cnt;
+  }
+
+  // Delete key's memory
 
   return 0;
 }
