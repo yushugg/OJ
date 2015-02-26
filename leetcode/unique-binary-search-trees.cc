608fce919a18f15a59df7099c6034165f5c2d2aa
yushugg <mahuachungg@gmail.com>
2015-02-26 17:09:43 +0800

    unique-binary-search-trees added, use DP, consider when i is root, left and right recursively

--- leetcode/unique-binary-search-trees.cc
+++ leetcode/unique-binary-search-trees.cc
@@ -1,19 +1,38 @@
 #include <cstdio>
+#include <cstring>
 
 
 class Solution {
 public:
   int numTrees(int n) {
-    return n;
+    int* G = new int[n + 1];
+    memset(G, 0, (n + 1) * sizeof(int));
+    G[0] = G[1] = 1;
+
+    for (int i = 2; i <= n; ++i)
+    {
+      for (int j = 1; j <= i; ++j)
+      {
+        G[i] += G[j - 1] * G[i - j];
+      }
+    }
+
+    int result = G[n];
+
+    delete[] G;
+
+    return result;
   }
 };
 
 
 int main(int argc, char* argv[])
 {
-  Solution solution;
-  int result = solution.numTrees(3);
+  int n;
+  scanf("%d", &n);
 
+  Solution solution;
+  int result = solution.numTrees(n);
   printf("%d\n", result);
 
   return 0;
