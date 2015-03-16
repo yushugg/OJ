9876438763018fddbd5f9ea2ee661e6965c039c8
yushugg <mahuachungg@gmail.com>
2015-03-16 21:11:10 +0800

    unique-binary-search-trees-ii added, use vector<TreeNode*> as return value

--- leetcode/unique-binary-search-trees-ii.cc
+++ leetcode/unique-binary-search-trees-ii.cc
@@ -9,7 +9,36 @@
  */
 class Solution {
 public:
-  vector<TreeNode *> generateTrees(int n) {
+  std::vector<TreeNode*> treesFromTo(int start, int end)
+  {
+    std::vector<TreeNode*> tree;
+    if (start > end)
+    {
+      tree.push_back(NULL);
+      return tree;
+    }
 
+    for (int mid = start; mid <= end; ++mid)
+    {
+      std::vector<TreeNode*> left = treesFromTo(start, mid - 1);
+      std::vector<TreeNode*> right = treesFromTo(mid + 1, end);
+
+      for (auto leftIter = left.begin(); leftIter != left.end(); ++leftIter)
+      {
+        for (auto rightIter = right.begin(); rightIter != right.end(); ++rightIter)
+        {
+          TreeNode* node = new TreeNode(mid);
+          node->left = *leftIter;
+          node->right = *rightIter;
+          tree.push_back(node);
+        }
+      }
+    }
+
+    return tree;
+  }
+
+  std::vector<TreeNode *> generateTrees(int n) {
+    return treesFromTo(1, n);
   }
 };
