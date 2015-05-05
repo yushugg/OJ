50ef3b39596146654dce3f7d969db1a73ab7d4fc
yushugg <mahuachungg@gmail.com>
2015-05-06 00:56:02 +0800

    word-search added, backtracking

--- leetcode/word-search.cc
+++ leetcode/word-search.cc
@@ -1,40 +1,36 @@
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <unordered_map>
 #include <iostream>
 
 using namespace std;
 
-struct Item {
-  int x;
-  int y;
-  bool visited;
-};
-
 class Solution {
 public:
+  bool visit(vector<vector<char> >& board, int x, int y, int count, const string& word) {
+    if (count == word.length()) return true;
+    if (x < 0) return false;
+    if (x >= board.size()) return false;
+    if (y < 0) return false;
+    if (y >= board[0].size()) return false;
+    if (board[x][y] != word[count]) return false;
+
+    char c = board[x][y];
+    board[x][y] = '.';
+    bool result = visit(board, x - 1, y, count + 1, word) || visit(board, x + 1, y, count + 1, word) || visit(board, x, y - 1, count + 1, word) || visit(board, x, y + 1, count + 1, word);
+    board[x][y] = c;
+    return result;
+  }
+
   bool exist(vector<vector<char>>& board, string word) {
-    unordered_multimap<char, Item> map;
+    if (word.empty()) return true;
     for (int i = 0; i < board.size(); ++i) {
       for (int j = 0; j < board[i].size(); ++j) {
-        Item item;
-        item.x = i;
-        item.y = j;
-        item.visited = false;
-        map.insert(make_pair(board[i][j], item));
+        if (board[i][j] == word[0]) {
+          if (visit(board, i, j, 0, word)) return true;
+        }
       }
     }
-
-    for (int i = 0; i < word.length(); ++i) {
-      auto range = map.equal_range(word[i]);
-      for (auto iter = range.first; iter != range.second; ++iter) {
-        cout << iter->second.x << "," << iter->second.y << ": " << iter->second.visited << " ";
-      }
-      cout << endl;
-    }
-
-    return true;
+    return false;
   }
 };
 
