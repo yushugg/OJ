ee86483e11096f3bf3409f0f563c27741d5df388
yushugg <mahuachungg@gmail.com>
2015-07-16 23:46:46 +0800

    delete-node-in-a-linked-list added

---
+++ leetcode/palindrome-partitioning-ii.cc
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  int minCut(string s) {
+    // abcbcb
+  }
+};
+
+int main() {
+  string s;
+  Solution solution;
+  while (cin >> s) cout << solution.minCut(s) << endl;
+
+  return 0;
+}
+
